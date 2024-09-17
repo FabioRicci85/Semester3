710ad2aff291c8e3a52597591524b5123607689d
FabioRicci85 <12300113@student.pxl.be>
2024-09-17 10:55:55 +0200

    innitiele commit semester 3

---
+++ Semester 3/C_Sharp Web/Oefeningen/Klassikale Oefeningen/MVCCoreEmpty/MVCCoreEmpty/Pages/Fabio.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MVCCoreEmpty.Pages
+{
+    public class FabioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
