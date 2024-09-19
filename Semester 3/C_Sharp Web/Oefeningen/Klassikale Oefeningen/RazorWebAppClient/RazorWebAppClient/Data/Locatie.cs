414ce9d0eba88904e826699e255c364a24f1208e
FabioRicci85 <12300113@student.pxl.be>
2024-09-19 21:08:00 +0200

    toevoeging van les 2 CSharp Web1

---
+++ Semester 3/C_Sharp Web/Oefeningen/Klassikale Oefeningen/RazorWebAppClient/RazorWebAppClient/Data/Locatie.cs
@@ -0,0 +1,16 @@
+﻿namespace RazorWebAppClient.Data
+{
+    public class Locatie
+    {
+        public int LocatieID { get; set; }
+        public int Postcode { get; set; }
+        public string Gemeente { get; set; }
+
+        public Locatie(int id, int postcode, string gemeente)
+        {
+            LocatieID = id;
+            Postcode = postcode;
+            Gemeente = gemeente;
+        }
+    }
+}
